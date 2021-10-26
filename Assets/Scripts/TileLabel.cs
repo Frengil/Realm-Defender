e1e04e85fa9160a63b1cb4cb698aed773b18f2b1
Fabian Mauz <fmauz@ipb-halle.de>
2021-10-26 20:17:19 +0200

    Add stuff for lesson 4 and 5
     * Coordinates of tiles

---
+++ Assets/Scripts/TileLabel.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[ExecuteAlways]
+public class TileLabel : MonoBehaviour{
+    [SerializeField]
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+  
+    void Awake() {        
+        
+        displayCoordinates();
+        updateTileName();
+    }
+   
+    void Update(){
+        if (!Application.isPlaying) {
+            displayCoordinates();
+            updateTileName();
+        }
+    }
+
+    void displayCoordinates() {        
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);      
+        label.text = coordinates.ToString();
+    }
+
+    void updateTileName() {
+        this.transform.parent.name= coordinates.ToString();
+    }
+}
