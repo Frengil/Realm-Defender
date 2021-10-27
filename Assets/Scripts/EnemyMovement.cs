0fe3703e8ff9b7ababc306c1343fc4d542834463
Fabian Mauz <fmauz@ipb-halle.de>
2021-10-27 09:24:23 +0200

    Add stuff of lesson 6
     * materials and preperation for pathfining

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour{
+    [SerializeField]
+    List<Waypoint> path = new List<Waypoint>();
+
+    private void Start() {
+        printPath();
+    }
+
+    void printPath() {
+        foreach(Waypoint wp in path) {
+            print(wp.transform.name);
+        }
+    }
+}
