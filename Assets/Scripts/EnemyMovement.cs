534970c372f746f130274e603a62274a1a1d9bb7
Fabian Mauz <fmauz@ipb-halle.de>
2021-10-28 00:20:34 +0200

    Lesson 9
     * smoothing enemy movement

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,7 +6,8 @@ public class EnemyMovement : MonoBehaviour{
     [SerializeField]
     List<Waypoint> path = new List<Waypoint>();
     [SerializeField]
-    float waitTime = 1;
+    [Range(0,5)]
+    float speed = 1;
 
     private void Start() {
         StartCoroutine(printPath());
@@ -14,8 +15,17 @@ public class EnemyMovement : MonoBehaviour{
 
     IEnumerator printPath() {
         foreach(Waypoint wp in path) {
-            this.transform.position = wp.transform.position;
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startPosition = this.transform.position;
+            Vector3 endPosition = wp.transform.position;
+            float travelPercent = 0f;
+            transform.LookAt(endPosition);
+            while (travelPercent < 1) {
+                travelPercent += Time.deltaTime*speed;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return new WaitForEndOfFrame();
+            }
+           
+
         }
     }
 }
