bd9f0fdd7b2e6feb818902449fbf2e6a2097caf4
Fabian Mauz <fmauz@ipb-halle.de>
2021-10-28 10:17:22 +0200

    Lesson 10
     * mouse clicks

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waypoint : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Waypoint : MonoBehaviour{
+    [SerializeField]
+    bool isPlaceable;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void OnMouseDown() {
+        if (isPlaceable) {
+            print(transform.name);
+        }
     }
 }
