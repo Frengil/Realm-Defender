170a29d9b3d819c702d0b88f2454bc48f86b67fc
Fabian Mauz <fmauz@ipb-halle.de>
2021-10-29 15:04:34 +0200

    Lesson 12 - Damaging Enemies
     * add particle system to shoot bolts
     * add method at enemy to register collisions and handle health

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour {
+    [SerializeField]
+    int maxHitPoints = 5;
+    int currentHitPoints = 0;
+    // Start is called before the first frame update
+    void Start() {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnParticleCollision(GameObject other) {
+        processHit();
+    }
+
+    void processHit() {
+        currentHitPoints--;
+        if (currentHitPoints < 1) {
+            Destroy(gameObject);
+        }
+    }
+}
