0718954ab21b933c5174eb89bef62fcb4e745668
Fabian Mauz <fmauz@ipb-halle.de>
2021-10-30 19:53:37 +0200

    Lesson 15 - Creating enemies by script

---
+++ Assets/Enemies/EnemyPool.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : MonoBehaviour{
+    [SerializeField]
+    private float spawntime = 1f;
+    [SerializeField]
+    private GameObject enemyPrefab;
+    void Start(){
+        StartCoroutine(spawnEnemies());
+    }
+
+    IEnumerator spawnEnemies() {
+        while (true) {
+            GameObject.Instantiate(enemyPrefab, this.transform);
+            yield return new WaitForSeconds(spawntime);
+        }
+    }
+}
