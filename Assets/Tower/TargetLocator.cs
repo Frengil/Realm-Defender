51cc98964e48ed08340fff8eadaad8e2386b9f46
Fabian Mauz <fmauz@ipb-halle.de>
2021-10-31 20:16:51 +0100

    Lesson 17 - Target closest enemy

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -6,17 +6,44 @@ public class TargetLocator : MonoBehaviour{
     [SerializeField]
     GameObject weapon;  
     Transform target;
+    [SerializeField]
+    float range = 15f;
+    [SerializeField]
+    ParticleSystem particelSystem;
     void Start(){
-        target= FindObjectOfType<EnemyMovement>().transform; 
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        aimWeapon();   
+        aimWeapon();
+        chooseTarget();
     }
 
     void aimWeapon() {
-        weapon.transform.LookAt(target);
+        if (target != null) {
+            float targetDistance = Vector3.Distance(this.transform.position, target.transform.position);
+            attack(targetDistance <= range);
+            weapon.transform.LookAt(target);
+        }else {
+            attack(false);
+        }
+    }
+
+    void chooseTarget() {
+        target = null;
+        float minimumDistance = Mathf.Infinity;
+        Enemy[] enemies = GameObject.FindObjectsOfType<Enemy>();
+        foreach(Enemy enemy in enemies) {
+            if (Vector3.Distance(enemy.transform.position, this.transform.position) < minimumDistance) {
+                minimumDistance = Vector3.Distance(enemy.transform.position, this.transform.position);
+                target = enemy.gameObject.transform;
+            }
+        }
+    }
+
+    void attack(bool isActive) {
+        particelSystem.enableEmission = isActive;
     }
 }
