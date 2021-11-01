047329d59d1a2f8ad5f44820fe9a200fc963980f
Fabian Mauz <fmauz@ipb-halle.de>
2021-11-01 20:35:34 +0100

    Lesson 19 - Currency II
     * towers now cost money

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour{
-    [SerializeField]
-    int startingBalance = 150;
-    public int currentBalance { private set; get; }
+   [SerializeField]
+   int startingBalance = 150;
+   public int currentBalance { private set; get; }
    public void deposit(int value) {
         currentBalance += Mathf.Abs(value);
     }
     public void withdraw(int value) {
         currentBalance -= Mathf.Abs(value);
+        if (currentBalance < 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
     private void Awake() {
---
+++ Assets/Tower/Tower.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour{
+    [SerializeField]
+    int cost=25;
+    Bank bank;
+    void Awake(){
+        bank = FindObjectOfType<Bank>();
+    }
+
+    void Update(){
+        
+    }
+
+    public bool createTower(Tower tower,Vector3 position) {
+        bank = FindObjectOfType<Bank>();
+        print(bank);
+        print(bank.currentBalance);
+        if (bank != null) {
+            if (bank.currentBalance >= cost) {
+                Instantiate(tower, position, Quaternion.identity);
+                bank.withdraw(cost);
+                return true;
+            }
+
+        }
+        return false;
+    }
+}
