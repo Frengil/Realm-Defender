432576a36787dc15c7dac8a8c345c27adaf8c490
Fabian Mauz <fmauz@ipb-halle.de>
2021-11-02 21:29:54 +0100

    Lesson 20 - UI Text
     * Add 3 fonts and show balance by ui text

--- Assets/Bank/Bank.cs
+++ Assets/Bank/Bank.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Bank : MonoBehaviour{
    [SerializeField]
    int startingBalance = 150;
+   [SerializeField]
+   TextMeshProUGUI balancetext;
    public int currentBalance { private set; get; }
    public void deposit(int value) {
         currentBalance += Mathf.Abs(value);
+        updateUI();
     }
     public void withdraw(int value) {
         currentBalance -= Mathf.Abs(value);
         if (currentBalance < 0) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
+        updateUI();
     }
 
     private void Awake() {
         currentBalance = startingBalance;
+        updateUI();
+    }
+
+    void updateUI() {
+        balancetext.text = "Gold: " + currentBalance;
     }
 
 }
