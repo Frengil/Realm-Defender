a4833e044efe30cd0c6e6b64bfe839dc0e8a90e3
Fabian Mauz <fmauz@ipb-halle.de>
2021-11-05 21:11:26 +0100

    Lesson 29 and 30

--- Assets/Pathfinding/GridManager.cs
+++ Assets/Pathfinding/GridManager.cs
@@ -2,17 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class GridManager : MonoBehaviour {
+    [SerializeField]
+    Vector2Int gridSize;
+    Dictionary<Vector2Int, Node> grid = new Dictionary<Vector2Int, Node>();
+
+    private void Awake() {
+        initGrid();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public Node getNode(Vector2Int coordinates) {
+        if (grid.ContainsKey(coordinates)) {
+            return grid[coordinates];
+        }
+        return null;
+    }
+
+    void initGrid() {
+        for (int x = 0; x < gridSize.x; x++) {
+            for (int y = 0; y < gridSize.y; y++) {
+                grid.Add(new Vector2Int(x, y), new Node(new Vector2Int(x, y), true));
+            }
+        }
     }
 }
--- Assets/World/TileLabel.cs
+++ Assets/World/TileLabel.cs
@@ -12,13 +12,17 @@ public class TileLabel : MonoBehaviour{
     [SerializeField]
     Color blockedColor = Color.gray;
     [SerializeField]
+    Color exploredColor = Color.blue;
+    [SerializeField]
+    Color pathColor = Color.red;
+    [SerializeField]
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    [SerializeField]
-    Waypoint waypoint;
+
+    GridManager gridManager;
   
     void Awake() {
-        waypoint = GetComponentInParent<Waypoint>();
+        gridManager = FindObjectOfType<GridManager>();
         displayCoordinates();
         updateTileName();
        
@@ -37,11 +41,31 @@ public class TileLabel : MonoBehaviour{
         coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
         coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);      
         label.text = coordinates.ToString();
-        label.color = waypoint.IsPlaceable ? defaultColor : blockedColor;
+        if (gridManager == null) {
+            return;
+        }                
     }
 
     void updateTileName() {
         this.transform.parent.name= coordinates.ToString();
+    }
+
+    void setLabelColor() {
+        Node node = gridManager.getNode(coordinates);
+        if (node == null) {
+            return;
+        }
+        if (!node.isWalkable) {
+            label.color = blockedColor;
+        }
+        else if (node.isExplored) {
+            label.color = exploredColor;
+        }
+        else if (node.isPath) {
+            label.color = pathColor;
+        } else {
+            label.color = defaultColor;
+        }
     }
 
     void toogleCoordinates() {
