7c8bb564feec863b468e82b2162e048a8b449b1f
Fabian Mauz <fmauz@ipb-halle.de>
2021-11-11 21:35:10 +0100

    Lesson 38 - Build Timer

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class Tower : MonoBehaviour{
     [SerializeField]
     int cost=25;
+    [SerializeField]
+    private float buildTime = 1f;
     Bank bank;
     void Awake(){
         bank = FindObjectOfType<Bank>();
     }
 
-    void Update(){
-        
+    private void Start() {
+        StartCoroutine(build());
     }
 
+    IEnumerator build() {
+        foreach (Transform child in transform) {
+            child.gameObject.SetActive(false);
+            foreach (Transform grandchild in child) {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform) {
+            yield return new WaitForSeconds(buildTime);
+            child.gameObject.SetActive(true);
+            foreach (Transform grandchild in child) {
+                grandchild.gameObject.SetActive(true);
+            }
+                
+        }
+    }
+     
     public bool createTower(Tower tower,Vector3 position) {
         bank = FindObjectOfType<Bank>();        
         if (bank != null) {
